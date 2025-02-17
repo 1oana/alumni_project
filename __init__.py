eea6d59c908e800b064bc7e91943a36ebbd8dc8f
ioanaduta <duta.ioana@gmail.com>
2025-02-17 14:53:03 +0100

    adding init

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# This file indicates that the directory is a Python package.
+# You can also use this file to initialize package-level variables or import submodules.
+
+# Example of importing a submodule
+# from . import submodule_name
+
+# Example of initializing a package-level variable
+# package_variable = "value"
